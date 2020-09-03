8439cd35eaf63d6033463aea54d617b638d81e23
jyouj <youj.whenever.prime3@gmail.com>
2020-09-04 01:11:25 +0900

    dammy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'Google', content: 'Gmail, YouTube, Google Drive'},
+        {id: '2', title: 'Amazon', content: 'AWS, Amazon Go'},
+        {id: '3', title: 'Facebook', content: 'Instagram, Whatsapp, Oculus'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
