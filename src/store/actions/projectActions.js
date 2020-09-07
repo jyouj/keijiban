2192e620ec971ffaa8ca9fd367e8e02882f701b7
jyouj <youj.whenever.prime3@gmail.com>
2020-09-07 18:40:57 +0900

    create project to Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,17 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
-        dispatch({ type: 'CREATE_PROJECT', project})
+        // make async call to database
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Suzu',
+            authorLastName: 'Hirose',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project})
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err})
+        })
     }
-}
+};
