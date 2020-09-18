8b00b2d21f202e9e0644723b9e43bc1eba596290
jyouj <youj.whenever.prime3@gmail.com>
2020-09-18 18:14:34 +0900

    create signup function

--- src/components/auth/SignUp/SignUp.js
+++ src/components/auth/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { signUp } from '../../../store/actions/authActions';
 
 class SignUp extends Component {
     state = {
@@ -16,11 +17,11 @@ class SignUp extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.signUp(this.state);
     }
 
     render () {
-        const { auth } = this.props;
+        const { auth, authError } = this.props;
         if (auth.uid) return <Redirect to='/' />
         
         return(
@@ -41,10 +42,13 @@ class SignUp extends Component {
                     </div>
                     <div className="input-field">
                         <label htmlFor="firstName">名前</label>
-                        <input type="password" id="firstName" onChange={this.handleChange} />
+                        <input type="text" id="firstName" onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">登録</button>
+                        <div className="red-text center">
+                            { authError ? <p>{authError}</p> : null }
+                        </div>
                     </div>
                 </form>
             </div>
@@ -54,8 +58,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
     }
 }
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
