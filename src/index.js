6d1ac7f0b7d88e66f1c43bf7f2588cc0e346f2ee
jyouj <youj.whenever.prime3@gmail.com>
2020-09-21 09:11:29 +0900

    complete successfully

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig, {attachAuthIsReady: true, useFirestoreForProfile: true, userProfile: 'users'})
+    reactReduxFirebase(fbConfig, {useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true})
   )
 );
 
